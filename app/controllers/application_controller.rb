108807fb0674de56e6a81518d07734df3a0cba33
Denise Pendleton <dpendl1@hotmail.com>
2018-04-19 17:31:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,9 @@ class ApplicationController < Sinatra::Base
 	post "/login" do
 		#your code here!
 		user = User.find_by(:username => params[:username])
+		if user
+			redirect '/success'
+		elseredirect '/failure'
 	end
 
 	get "/success" do
